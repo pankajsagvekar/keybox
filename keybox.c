82aab947738b4a744b35893ea19f9160bf305717
Pankaj Sagvekar <pankajsagvekar167@gmail.com>
2025-05-31 01:11:16 +0530

    initial build logic

---
+++ keybox.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MAX 100
+
+typedef struct KeyValue{
+	char key[50];
+	char value[100];
+}KeyValue;
+
+KeyValue store[MAX];
+int count = 0;
+
+int main(){
+	return 0;
+}
